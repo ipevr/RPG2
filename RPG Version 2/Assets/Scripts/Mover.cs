736838e153b0a5517a3120593f902f1e97fb4580
Ingo Peters <ingo.peters2@gmx.de>
2019-03-07 21:49:20 +0100

    - Start sandbox

---
+++ RPG Version 2/Assets/Scripts/Mover.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour {
+
+    NavMeshAgent agent = null;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            MoveToCursor();
+        }
+    }
+
+    void MoveToCursor() {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit)) {
+            agent.destination = hit.point;
+        }
+    }
+}
